51e57ef879e224d98a539ee2f8a72f2180ad7c1d
ArtMed <artiebears@mail.ru>
2023-02-18 11:31:51 +0300

    new func and file

--- source/main.cpp
+++ source/main.cpp
@@ -3,58 +3,12 @@
 #include <cmath>
 #include "../utils/files.h"
 #include "../utils/constants.h"
+#include "../utils/solver.h"
 
-
-#define ind(x,y,z) x+y*Nx+z*Nx*Ny
 
 #define SAVE_SEPARATED_MESH true
 #define SAVE_ALL_MESH true
 
-/* Calculate transitivity between 2 neighbouring cells */
-double computeTransability(
-        int x, int y, int z, std::string axis,
-        std::vector<double> &kx,
-        std::vector<double> &ky,
-        std::vector<double> &kz) {
-    double prevK, currK;
-    if (axis == "x") {
-        prevK = kx[ind(x-1,y,z)]; //(x - 1) + y * Nx + z * Nx * Ny
-        currK = kx[ind(x,y,z)];  //x + y * Nx + z * Nx * Ny
-    } else if (axis == "y") {
-        prevK = ky[ind(x,y-1,z)];   //x + (y - 1) * Nx + z * Nx * Ny
-        currK = ky[ind(x,y,z)];
-    } else if (axis == "z") {
-        prevK = kz[ind(x,y,z-1)];
-        currK = kz[ind(x,y,z)];
-        std::cerr << "Only 2D examples now!" << std::endl;
-        throw;
-    } else {
-        std::cerr << "Axis Error";
-        throw;
-    }
-    return (2*prevK*currK/(prevK+currK));
-}
-
-
-
-/* Save the A-matrix as ["../data/A.mtx" = default]*/
-void save2DMatrix(
-        std::vector<double> &A,
-        int shapeX = Nx, int shapeY = Ny, int shapeZ = Nz,
-        std::string filename = "../data/A.mtx"
-) {
-    std::ofstream file;
-    file.open(filename);
-
-    for (int y = 0; y < shapeX; ++y) // Is it shapeX? mb no?
-    {
-        for (int x = 0; x < shapeX; ++x) {
-            file << A[x] << " ";
-        }
-        file << std::endl;
-    }
-    file.close();
-}
 
 /* Main algorithm that constructs the A-matrix*/
 void createMatrix(
@@ -64,24 +18,26 @@ void createMatrix(
         std::vector<double> &ky,
         std::vector<double> &kz) {
 //    for (int z = 0; z < Nz + 1; ++z)    //<---uncomment this when move on 3d model
-        int z=50;  //our layer            //<---  comment this when move on 3d model
-        for (int y = 0; y < Ny + 1; ++y)
-            for (int x = 0; x < Nx + 1; ++x) {
+    int z=50;  //our layer            //<---  comment this when move on 3d model
+    for (int y = 0; y < Ny + 1; ++y)
+        for (int x = 0; x < Nx + 1; ++x) {
 
-                if ((x == 0) || (y == 0) || (x == Nx) || (y == Ny))
-                    continue;      // boundary condition (have already been filled with 0)
+            if ((x == 0) || (y == 0) || (x == Nx) || (y == Ny))
+                continue;      // boundary condition (have already been filled with 0)
 
-                double T_xp1_x = computeTransability(x + 1, y, z, "x", kx, ky, kz);  // T (x + 1, x)
-                double T_x_xm1 = computeTransability(x, y, z, "x", kx, ky, kz);      // T (x, x - 1)
+            double T_xp1_x = computeTransability(x + 1, y, z, "x", kx, ky, kz);  // T (x + 1, x)
+            double T_x_xm1 = computeTransability(x, y, z, "x", kx, ky, kz);      // T (x, x - 1)
 
-                double T_yp1_y = computeTransability(x, y + 1, z, "y", kx, ky, kz);  // T (y + 1, y)
-                double T_y_ym1 = computeTransability(x, y, z, "y", kx, ky, kz);      // T (y, y - 1)
+            double T_yp1_y = computeTransability(x, y + 1, z, "y", kx, ky, kz);  // T (y + 1, y)
+            double T_y_ym1 = computeTransability(x, y, z, "y", kx, ky, kz);      // T (y, y - 1)
 
 
-                /*TODO*/
+            /*TODO*/
 
-            }
+        }
 }
+
+
 
 /* Run all functions */
 int main(int argc, char **argv) {
--- utils/constants.h
+++ utils/constants.h
@@ -2,3 +2,6 @@
 #define Nx (60)
 #define Ny (220)
 #define Nz (85)
+#define hx (1) //step by x axis
+#define hy (1) //step by y axis
+#define hz (1) //step by z axis
